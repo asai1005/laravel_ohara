eb444a55b00168624342a3e902752ac1da74e125
Asai <M2420034@ngo.ohara.ac.jp>
2025-06-30 12:40:01 +0900

    0630MVCモデル

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
+// use文。このファイル内で扱うクラスをインポートするための機能
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
+//テスト用ルーティング
+// ルートには名前を付けることができる。名前を付けておくと、リンクを貼るときに便利。
+Route::get('tests/test', [TestController::class, 'index'])->name('test.index');
+
+
+//ルーティングの定義 Route::通信方法(post or get) (アドレス、処理);
+//ex.「/」というURLにアクセスしたら return view('welcome')という処理を行う
+//view(ビューファイル)でビューファイルを表示。自動的にresources.viewsの中身を参照し、welcome.blade.phpのファイ名のみで指定できる
 Route::get('/', function () {
     return view('welcome');
 });
 
+//middleware(auth)→認証機能。ログインしているユーザーでなければdashboardにはアクセスできない。
+//ログインしていないユーザーがアクセスするとログイン画面にリダイレクトがかかる
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +29,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
